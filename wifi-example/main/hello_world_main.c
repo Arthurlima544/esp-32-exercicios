9d79029a7dc606b6b7a2f622b4ee21dde41b0db1
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-06-27 13:57:43 -0300

    add wifi example

---
+++ wifi-example/main/hello_world_main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "freertos/semphr.h"
+#include "esp_log.h"
+
+
+#include "wifi.h"
+#include "http_client.h"
+
+SemaphoreHandle_t conexaoWifiSemaphore;
+
+void RealizaHTTPRequest(void * params)
+{
+  while(true)
+  {
+    if(xSemaphoreTake(conexaoWifiSemaphore, portMAX_DELAY))
+    {
+      ESP_LOGI("Main Task", "Realiza HTTP Request");
+      //http_request();
+      https_request();
+    }
+  }
+}
+
+void app_main(void)
+{
+    // Inicializa o NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    
+    conexaoWifiSemaphore = xSemaphoreCreateBinary();
+    wifi_start();
+
+    xTaskCreate(&RealizaHTTPRequest,  "Processa HTTP", 4096, NULL, 1, NULL);
+    
+}
+
